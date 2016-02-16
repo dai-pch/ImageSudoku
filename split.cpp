9b05f765ee96c7fda4ad94b9af86cfbe111b8382
Dai, PengCheng <dpc_work@163.com>
2016-02-16 11:22:16 +0800

    增加split中寻找外框部分

--- split.cpp
+++ split.cpp
@@ -13,6 +13,20 @@ using namespace std;
 	namedWindow("二值化图像");
 	imshow("二值化图像", binaryImage);*/
 
+	//检测轮廓
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(binaryImage, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+	//寻找矩形外框
+	int index = -1; //hierarchy.at(0)(2);
+	index = InterestContour(contours, hierarchy, 81);
+	if (index < 0)
+	{
+		throw logic_error("Can't find right contours.");
+	}
+
+	//提取出子边界
 
 
 }
